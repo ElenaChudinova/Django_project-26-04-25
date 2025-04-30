fdf05a195f025aa4947076a201a17b0a47700ba0
ovetganna <eovetganna@icloud.com>
2025-04-30 18:44:11 +0300

    new DZ 30 04 25

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'description', 'category_name')
+    list_filter = ('category_name',)
+    search_fields = ('product_name', 'description',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name', 'description')
+    list_filter = ('category_name',)
+    search_fields = ('category_name',)
+
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=50, verbose_name='Наименование') # наименование,
+    description = models.TextField(max_length=300, verbose_name='Описание', null=True, blank=True) # описание,
+    image = models.ImageField(upload_to='photos/', verbose_name='Фотография', null=True) # изображение,
+    category_name = models.CharField(max_length=50, verbose_name='Категория') # категория,
+    price = models.IntegerField(max_length=6, verbose_name='Цена') # цена за покупку,
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания', editable=False) # дата создания,
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения', editable=False) # дата последнего изменения.
+
+    def __str__(self):
+        return f'{self.product_name} {self.category_name}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ['product_name',]
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, verbose_name='Наименование')  # наименование,
+    description = models.TextField(max_length=300, verbose_name='Описание', null=True, blank=True)  # описание,
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='categories')
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ['category_name', ]
