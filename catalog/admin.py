da62cce7b315ece9df875d15732b2f255f4a28e0
ovetganna <eovetganna@icloud.com>
2025-05-01 15:17:18 +0300

    new update 01 05 25

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,13 +3,15 @@ from .models import Product, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('product_name', 'description', 'category_name')
+    idlelib = ('id',)
+    list_display = ('product_name', 'price', 'category_name')
     list_filter = ('category_name',)
     search_fields = ('product_name', 'description',)
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    idlelib = ('id',)
     list_display = ('category_name', 'description')
     list_filter = ('category_name',)
-    search_fields = ('category_name',)
+    search_fields = ('category_name', 'description',)
 
--- catalog/models.py
+++ catalog/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 
 class Product(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=True, verbose_name='id')
     product_name = models.CharField(max_length=50, verbose_name='Наименование') # наименование,
     description = models.TextField(max_length=300, verbose_name='Описание', null=True, blank=True) # описание,
-    image = models.ImageField(upload_to='photos/', verbose_name='Фотография', null=True) # изображение,
+    image = models.ImageField(upload_to='photos/', verbose_name='Фотография', null=True, blank=True) # изображение,
     category_name = models.CharField(max_length=50, verbose_name='Категория') # категория,
     price = models.IntegerField(max_length=6, verbose_name='Цена') # цена за покупку,
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания', editable=False) # дата создания,
@@ -20,6 +21,7 @@ class Product(models.Model):
 
 
 class Category(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=True, verbose_name='id')
     category_name = models.CharField(max_length=50, verbose_name='Наименование')  # наименование,
     description = models.TextField(max_length=300, verbose_name='Описание', null=True, blank=True)  # описание,
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='categories')
